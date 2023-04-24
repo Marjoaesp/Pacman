ecff1b7c81331a5cb0bce37eb0c9f8d63452054a
Marjoaesp <111077142+Marjoaesp@users.noreply.github.com>
2023-04-24 16:26:45 -0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -394,10 +394,9 @@ exampleSocket.onopen = (event) => {
 
     exampleSocket.send("server connected");
     const time = new Date();
-	let playerSocket=JSON.parse(localStorage.getItem("player"))
 
 	exampleSocket.send(JSON.stringify(playerSocket));
-	console.log("name"+ player.name, time, "Highscore: " + Highscore);
+	console.log(time, "Highscore: " + Highscore);
 
 
  };
