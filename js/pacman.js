e9d3154192b7fb1d277c9078ec3a03587b357f73
Marjoaesp <111077142+Marjoaesp@users.noreply.github.com>
2023-05-22 17:05:02 -0300

    fixed

--- js/pacman.js
+++ js/pacman.js
@@ -21,6 +21,37 @@ var PACMAN_RETRY_SPEED = 2100;
 var PACMAN_DEAD = false;
 var GAMMA;
 var BETA;
+
+window.addEventListener('deviceorientation', handleOrientation);
+
+function handleOrientation(event) {
+  const alpha = event.alpha;
+  const beta = event.beta;
+  const gamma = event.gamma;
+  
+  GAMMA=gamma;
+  BETA=beta;
+if (GAMMA>=30 ) { 
+	console.log("Moveright")
+	movePacman(1);
+
+} else if ( GAMMA>= -30  ) { 
+	movePacman(2);
+
+	console.log("Moveleft")
+
+} else if ( BETA>= 30 ) 
+{ 
+	movePacman(3);
+
+	console.log("Moveup")
+
+} else if ( BETA>= -30 ) { 
+	console.log("Movedowm")
+	movePacman(4);
+
+}
+}
 function initPacman() { 
 	var canvas = document.getElementById('canvas-pacman');
 	canvas.setAttribute('width', '550');
@@ -170,17 +201,7 @@ function movePacman(direction) {
 		tryMovePacmanCancel();
 	}
 }
-window.addEventListener('deviceorientation', handleOrientation);
 
-function handleOrientation(event) {
-  const alpha = event.alpha;
-  const beta = event.beta;
-  const gamma = event.gamma;
-  GAMMA=event.gamma;
-  BETA=event.beta;
-  console.log(GAMMA, BETA);
-  return GAMMA, BETA;
-}
 
 
 
