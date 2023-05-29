1cb83ff32a4d50d0e4864386d23db0996da0fbe3
Marjoaesp <111077142+Marjoaesp@users.noreply.github.com>
2023-05-29 18:42:04 -0300

    pinch

--- js/Pincj.js
+++ js/Pincj.js
@@ -1,40 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-   // Verificar si el dispositivo móvil es compatible con eventos táctiles
-    // Obtener el elemento en el que deseas detectar el gesto de "pinch"
-    var pinchElement = document.getElementById('elemento');
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    var pinchElement = document.getElementById('contenedor');
+    var circleElement = document.getElementById('circle');
   
-    // Variables para almacenar la escala y el estado inicial
     var initialDistance = 0;
     var initialScale = 1;
   
-    // Función para manejar el evento de "pinch"
     function handlePinch(event) {
-      // Verificar que haya al menos dos dedos en el evento táctil
       if (event.touches.length >= 2) {
-        // Obtener las coordenadas de los dos primeros dedos
         var touch1 = event.touches[0];
         var touch2 = event.touches[1];
-  
-        // Calcular la distancia actual entre los dos dedos
         var distance = Math.hypot(touch2.pageX - touch1.pageX, touch2.pageY - touch1.pageY);
   
-        // Si es la primera vez que se realiza el gesto de "pinch", almacenar la distancia y escala inicial
         if (initialDistance === 0) {
           initialDistance = distance;
-          initialScale = parseFloat(pinchElement.style.transform.replace('scale(', '').replace(')', ''));
+          initialScale = 0;
         }
   
-        // Calcular la escala actual basada en la distancia actual y la distancia inicial
-        var scale = initialScale * (distance / initialDistance);
-  
-        // Aplicar la escala al elemento
-        pinchElement.style.transform = 'scale(' + scale + ')';
+        var scale = initialScale + (distance - initialDistance) / 100;
+        circleElement.style.transform = 'translate(-50%, -50%) scale(' + scale + ')';
       }
     }
   
-    // Agregar el evento de "pinch" al elemento
+    function handleRelease(event) {
+      if (event.touches.length === 0) {
+        initialDistance = 0;
+        initialScale = 1;
+        circleElement.style.transform = 'translate(-50%, -50%) scale(0)';
+      }
+    }
+  
     pinchElement.addEventListener('touchmove', handlePinch);
-
-  });
-
+    pinchElement.addEventListener('touchend', handleRelease);
+  }
