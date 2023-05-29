f16e5a1533ade440c82da8f60014ea4c55ca1839
Marjoaesp <111077142+Marjoaesp@users.noreply.github.com>
2023-05-29 18:31:01 -0300

    base

--- js/Pincj.js
+++ js/Pincj.js
@@ -1,53 +1,37 @@
-var element = document.getElementById('#elemento');
+// Verificar si el dispositivo móvil es compatible con eventos táctiles
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    // Obtener el elemento en el que deseas detectar el gesto de "pinch"
+    var pinchElement = document.getElementById('#elemento');
+  
+    // Variables para almacenar la escala y el estado inicial
     var initialDistance = 0;
     var initialScale = 1;
-    var initialX = 0;
-    var initialY = 0;
-
-    // Función para calcular la distancia entre los primeros dos puntos de contacto
-    function calculateDistance(touches) {
-      var x1 = touches[0].clientX;
-      var y1 = touches[0].clientY;
-      var x2 = touches[1].clientX;
-      var y2 = touches[1].clientY;
-
-      return Math.hypot(x2 - x1, y2 - y1);
-    }
-
-    // Función para manejar el evento touchstart
-    function touchstartHandler(event) {
-      if (event.touches.length === 2) {
-        initialDistance = calculateDistance(event.touches);
-        initialScale = parseFloat(element.style.transform.replace('scale(', '')) || 1;
-        initialX = event.touches[0].clientX - element.offsetLeft;
-        initialY = event.touches[0].clientY - element.offsetTop;
+  
+    // Función para manejar el evento de "pinch"
+    function handlePinch(event) {
+      // Verificar que haya al menos dos dedos en el evento táctil
+      if (event.touches.length >= 2) {
+        // Obtener las coordenadas de los dos primeros dedos
+        var touch1 = event.touches[0];
+        var touch2 = event.touches[1];
+  
+        // Calcular la distancia actual entre los dos dedos
+        var distance = Math.hypot(touch2.pageX - touch1.pageX, touch2.pageY - touch1.pageY);
+  
+        // Si es la primera vez que se realiza el gesto de "pinch", almacenar la distancia y escala inicial
+        if (initialDistance === 0) {
+          initialDistance = distance;
+          initialScale = parseFloat(pinchElement.style.transform.replace('scale(', '').replace(')', ''));
+        }
+  
+        // Calcular la escala actual basada en la distancia actual y la distancia inicial
+        var scale = initialScale * (distance / initialDistance);
+  
+        // Aplicar la escala al elemento
+        pinchElement.style.transform = 'scale(' + scale + ')';
       }
     }
-
-    // Función para manejar el evento touchmove
-    function touchmoveHandler(event) {
-      event.preventDefault(); // Evita el desplazamiento de la página en dispositivos táctiles
-
-      if (event.touches.length === 2) {
-        var currentDistance = calculateDistance(event.touches);
-        var scale = (currentDistance / initialDistance) * initialScale;
-        element.style.transform = 'translate(-50%, -50%) scale(' + scale + ')';
-        
-        var currentX = event.touches[0].clientX - initialX;
-        var currentY = event.touches[0].clientY - initialY;
-        element.style.left = currentX + 'px';
-        element.style.top = currentY + 'px';
-      }
-    }
-
-    // Función para manejar el evento touchend
-    function touchendHandler(event) {
-      if (event.touches.length < 2) {
-        initialDistance = 0;
-        initialScale = 1;
-      }
-    }
-
-    element.addEventListener('touchstart', touchstartHandler, false);
-    element.addEventListener('touchmove', touchmoveHandler, false);
-    element.addEventListener('touchend', touchendHandler, false);
+  
+    // Agregar el evento de "pinch" al elemento
+    pinchElement.addEventListener('touchmove', handlePinch);
+}
